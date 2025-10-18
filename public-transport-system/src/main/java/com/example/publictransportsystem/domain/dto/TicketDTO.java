70c6d1b7d99cb8be78ddd19eea71872a57114a39
IvoRum <ivailo.rumenov@outlook.com>
2025-10-19 00:52:11 +0300

    Removed unused import

--- public-transport-system/src/main/java/com/example/publictransportsystem/domain/dto/TicketDTO.java
+++ public-transport-system/src/main/java/com/example/publictransportsystem/domain/dto/TicketDTO.java
@@ -1,6 +1,5 @@
 package com.example.publictransportsystem.domain.dto;
 
-import com.example.publictransportsystem.persitence.PassengerEntity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
